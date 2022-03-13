d6de8de734928ab3b32f40027c677bb000258923
Lisa Ye <lisa@Lisas-MacBook-Air.local>
2022-03-13 05:07:05 -0400

    updated friend screen nav WIP

--- Midterm/screen_nav/FriendsScreenNav.js
+++ Midterm/screen_nav/FriendsScreenNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-// import ToDoScreen from './ToDoScreen'
+import FriendsScreen from "../screens/FriendsScreen";
 // import ToDoDetailScreen from '../screens/ToDoDetailScreen'
 
 const Stack = createStackNavigator();
@@ -8,8 +8,8 @@ const Stack = createStackNavigator();
 export default function FriendsScreenNav() {
   return (
     <Stack.Navigator>
-      {/* <Stack.Screen name="To Dos" component={ToDoScreen} />
-      <Stack.Screen name="ToDoDetailScreen" component={ToDoDetailScreen} /> */}
+      <Stack.Screen name="FriendsList" component={FriendsScreen} />
+      {/* <Stack.Screen name="ToDoDetailScreen" component={ToDoDetailScreen} /> */}
     </Stack.Navigator>
   );
 }
